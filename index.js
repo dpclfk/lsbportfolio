192ddba0ba2e9076e53c3bb45dd5e4a4bee5cebf
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-12-10 13:52:44 +0900

    javascript end

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const modaldiv = document.getElementById("modal-div");
 const modalbg = document.getElementById("modal-bg");
-const modaltitle = document.getElementById("modal-title");
-const modalimg = document.getElementById("modal-img");
+const modaltitle = document.getElementById("modal-title-text");
+const modalimg = document.querySelector("#modal-img img");
 const modaltext = document.getElementById("modal-text");
 const modalexit = document.getElementById("exit-btn");
+const modalprojecticon = document.getElementById("modal-project-icon");
 
-const morebtn1 = document.getElementsByClassName("more-btn")[0];
-const morebtn2 = document.getElementsByClassName("more-btn")[1];
-const morebtn3 = document.getElementsByClassName("more-btn")[2];
+const morebtn = document.getElementsByClassName("more-btn");
+
+const test = document.getElementById("gnb1");
+test.onclick = () => {
+  location.reload();
+};
 
 history.scrollRestoration = "manual";
 
@@ -40,7 +44,8 @@ modalexit.onclick = () => {
   document.body.classList.remove("scroll-stop");
 };
 
-morebtn1.onclick = () => {
+morebtn[0].onclick = () => {
+  modalprojecticon.src = "./img/project/lost.ico";
   modaldiv.classList.remove("none");
   document.body.classList.add("scroll-stop");
   modaltitle.innerText = "로스트아크 계산기";
@@ -72,16 +77,16 @@ morebtn1.onclick = () => {
   </li>
 </ul>`;
 };
-morebtn2.onclick = () => {
+morebtn[1].onclick = () => {
+  modalprojecticon.src = "./img/project/market.ico";
   modaldiv.classList.remove("none");
   document.body.classList.add("scroll-stop");
   modaltitle.innerText = "햄스터 마켓";
-
   modalimg.onclick = () => {
     window.open(`https://market.dpclfk.com`);
   };
   modalimg.src = "./img/market.png";
-  modaltext.innerHTML = `<ul id="list-none">
+  modaltext.innerHTML = `<ul id="">
   <li class="pb-8">
     <span class="weight700 modal-text-title">맡은 역할</span>
     <ul class="modal-text-contents">
@@ -122,10 +127,48 @@ morebtn2.onclick = () => {
   </li>
 </ul>`;
 };
-morebtn3.onclick = () => {
+morebtn[2].onclick = () => {
+  modalprojecticon.src = "./img/project/Arcalive_logo.svg";
   modaldiv.classList.remove("none");
   document.body.classList.add("scroll-stop");
-  // modalimg.src = "./img/clone.png";
+  modaltitle.innerText = "아카라이브 클론코딩";
+  modalimg.onclick = () => {
+    window.open(`https://arcaclone.dpclfk.com`);
+  };
+  modalimg.src = "./img/clone.png";
+  modaltext.innerHTML = `<ul id="list-none">
+  <li class="pb-8">
+    <span class="weight700 modal-text-title">맡은 역할</span>
+    <ul class="modal-text-contents">
+      <li>ERD, API명세서, 백엔드 코드 작성</li>
+    </ul>
+  </li>
+  <li class="pb-8">
+    <span class="weight700 modal-text-title">기능 </span>
+    <ul class="modal-text-contents">
+      <li>MySQL을 사용하여, 게시글 및 유저정보 저장 및 관리</li>
+      <li>세션을 이용하여 로그인 기능 구현</li>
+    </ul>
+  </li>
+  <li class="pb-8">
+    <span class="weight700 modal-text-title">기간 </span>
+    <ul class="modal-text-contents">
+      <li>2024.05.20 ~ 2024.06.04</li>
+    </ul>
+  </li>
+  <li class="pb-8">
+    <span class="weight700 modal-text-title">일수 </span>
+    <ul class="modal-text-contents">
+      <li>15일</li>
+    </ul>
+  </li>
+  <li class="pb-8">
+    <span class="weight700 modal-text-title">인원 </span>
+    <ul class="modal-text-contents">
+      <li>3명</li>
+    </ul>
+  </li>
+</ul>`;
 };
 
 for (let i = 0; i < 4; i++) {
